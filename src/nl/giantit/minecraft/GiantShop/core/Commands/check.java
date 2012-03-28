2d46824c0329c6cd8b8747444eb20fae8bc19c90
Giant <giant@giantit.nl>
2012-03-29 00:29:45 +0200

    More work to check command, should be finished soon-ish

--- src/nl/giantit/minecraft/GiantShop/core/Commands/check.java
+++ src/nl/giantit/minecraft/GiantShop/core/Commands/check.java
@@ -7,12 +7,14 @@ import nl.giantit.minecraft.GiantShop.core.config;
 import nl.giantit.minecraft.GiantShop.core.perm;
 import nl.giantit.minecraft.GiantShop.core.Database.db;
 import nl.giantit.minecraft.GiantShop.core.Items.Items;
+import nl.giantit.minecraft.GiantShop.core.Items.ItemID;
 
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.logging.Level;
 
 /**
  *
@@ -26,7 +28,62 @@ public class check {
 		perm perms = perm.Obtain();
 		config conf = config.Obtain();
 		if(perms.has(player, "giantshop.shop.check")) {
+			int itemID;
+			Integer itemType = null;
+			
+			if(!args[1].matches("[0-9]+:[0-9]+")) {
+				try {
+					itemID = Integer.parseInt(args[1]);
+					itemType = null;
+				}catch(NumberFormatException e) {
+					ItemID key = iH.getItemIDByName(args[1]);
+					if(key != null) {
+						itemID = key.getId();
+						itemType = key.getType();
+					}else{
+						Heraut.say(player, msgs.getMsg(Messages.msgType.ERROR, "itemNotFound"));
+						return;
+					}
+				}catch(Exception e) {
+					if(conf.getBoolean("GiantShop.global.debug") == true) {
+						GiantShop.log.log(Level.SEVERE, "GiantShop Error: " + e.getMessage());
+						GiantShop.log.log(Level.INFO, "Stacktrace: " + e.getStackTrace());
+					}
+
+					Heraut.say(player, msgs.getMsg(Messages.msgType.ERROR, "unknown"));
+					return;
+				}
+			}else{
+				try {
+					String[] data = args[1].split(":");
+					itemID = Integer.parseInt(data[0]);
+					itemType = Integer.parseInt(data[1]);
+				}catch(NumberFormatException e) {
+					HashMap<String, String> data = new HashMap<String, String>();
+					data.put("command", "check");
+
+					Heraut.say(player, msgs.getMsg(Messages.msgType.ERROR, "syntaxError", data));
+					return;
+				}catch(Exception e) {
+					if(conf.getBoolean("GiantShop.global.debug") == true) {
+						GiantShop.log.log(Level.SEVERE, "GiantShop Error: " + e.getMessage());
+						GiantShop.log.log(Level.INFO, "Stacktrace: " + e.getStackTrace());
+					}
+
+					Heraut.say(player, msgs.getMsg(Messages.msgType.ERROR, "unknown"));
+					return;
+				}
+			}
+			
 			db DB = db.Obtain();
+			ArrayList<String> fields = new ArrayList<String>();
+			fields.add("perStack");
+			fields.add("sellFor");
+			fields.add("buyFor");
+			fields.add("stock");
+			fields.add("shops");
+			
+			DB.select(fields).where(null, true);
 		}else{
 			HashMap<String, String> data = new HashMap<String, String>();
 			data.put("command", "check");
