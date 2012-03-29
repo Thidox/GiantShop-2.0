cf71e73e4d3b30d98c5a3e771766ed6db7b253c3
Giant <giant@giantit.nl>
2012-03-29 04:11:20 +0200

    And that should be sufficient for the console version of check for now! :)

--- src/nl/giantit/minecraft/GiantShop/core/Commands/check.java
+++ src/nl/giantit/minecraft/GiantShop/core/Commands/check.java
@@ -130,5 +130,94 @@ public class check {
 		Items iH = GiantShop.getPlugin().getItemHandler();
 		config conf = config.Obtain();
 		
+		db DB = db.Obtain();
+		int itemID;
+		Integer itemType = -1;
+
+		if(!args[1].matches("[0-9]+:[0-9]+")) {
+			try {
+				itemID = Integer.parseInt(args[1]);
+				itemType = -1;
+			}catch(NumberFormatException e) {
+				ItemID key = iH.getItemIDByName(args[1]);
+				if(key != null) {
+					itemID = key.getId();
+					itemType = key.getType();
+				}else{
+					Heraut.say(sender, msgs.getConsoleMsg(Messages.msgType.ERROR, "itemNotFound"));
+					return;
+				}
+			}catch(Exception e) {
+				if(conf.getBoolean("GiantShop.global.debug") == true) {
+					GiantShop.log.log(Level.SEVERE, "GiantShop Error: " + e.getMessage());
+					GiantShop.log.log(Level.INFO, "Stacktrace: " + e.getStackTrace());
+				}
+
+				Heraut.say(sender, msgs.getConsoleMsg(Messages.msgType.ERROR, "unknown"));
+				return;
+			}
+		}else{
+			try {
+				String[] data = args[1].split(":");
+				itemID = Integer.parseInt(data[0]);
+				itemType = Integer.parseInt(data[1]);
+			}catch(NumberFormatException e) {
+				HashMap<String, String> data = new HashMap<String, String>();
+				data.put("command", "check");
+
+				Heraut.say(sender, msgs.getConsoleMsg(Messages.msgType.ERROR, "syntaxError", data));
+				return;
+			}catch(Exception e) {
+				if(conf.getBoolean("GiantShop.global.debug") == true) {
+					GiantShop.log.log(Level.SEVERE, "GiantShop Error: " + e.getMessage());
+					GiantShop.log.log(Level.INFO, "Stacktrace: " + e.getStackTrace());
+				}
+
+				Heraut.say(sender, msgs.getConsoleMsg(Messages.msgType.ERROR, "unknown"));
+				return;
+			}
+		}
+		itemType = (itemType == null || itemType == 0) ? -1 : itemType;
+
+		ArrayList<String> fields = new ArrayList<String>();
+		fields.add("perStack");
+		fields.add("sellFor");
+		fields.add("buyFor");
+		fields.add("stock");
+		fields.add("shops");
+
+		HashMap<String, String> where = new HashMap<String, String>();
+		where.put("itemID", String.valueOf(itemID));
+		where.put("type", String.valueOf(itemType));
+
+		ArrayList<HashMap<String, String>> resSet = DB.select(fields).from("#__items").where(where).execQuery();
+		if(resSet.size() == 1) {
+			//Wait didn't we just do this the other way round?!
+			//Yea we did! Why? Because we can!
+			itemType = (itemType == -1) ? 0 : itemType;
+
+			String name = iH.getItemNameByID(itemID, itemType);
+			HashMap<String, String> res = resSet.get(0);
+			Heraut.say(sender, "Here's the result for " + name + "!");
+			Heraut.say(sender, "ID: " + itemID);
+			Heraut.say(sender, "Type: " + itemType);
+			Heraut.say(sender, "Quantity per amount: " + res.get("perStack"));
+			Heraut.say(sender, "Leaves shop for: " + res.get("sellFor"));
+			Heraut.say(sender, "Retursns to shop for: " + res.get("buyFor"));
+			Heraut.say(sender, "Amount of items in he shop: " + (!res.get("stock").equals("-1") ? res.get("stock") : "unlimited"));
+			//More future stuff
+			/*if(conf.getBoolean("GiantShop.Location.useGiantShopLocation") == true) {
+			 *		ArrayList<Indaface> shops = GiantShop.getPlugin().getLocationHandler().parseShops(res.get("shops"));
+			 *		for(Indaface shop : shops) {
+			 *			if(shop.inShop(player.getLocation())) {
+			 *				Heraut.say(player, "Something about what shops these items are in or something like that!");
+			 *				break;
+			 *			}
+			 *		}
+			 * } 
+			 */
+		}else{
+			Heraut.say(sender, msgs.getConsoleMsg(Messages.msgType.ERROR, "noneOrMoreResults"));
+		}
 	}
 }
