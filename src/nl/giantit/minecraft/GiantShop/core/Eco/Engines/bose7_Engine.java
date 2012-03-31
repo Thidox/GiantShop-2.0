8aee46997da8be7ef5b3dbb541d4152d36c24011
Giant <giant@giantit.nl>
2012-03-31 17:23:12 +0200

    That should do for BOSE7

--- src/nl/giantit/minecraft/GiantShop/core/Eco/Engines/bose7_Engine.java
+++ src/nl/giantit/minecraft/GiantShop/core/Eco/Engines/bose7_Engine.java
@@ -1,12 +1,41 @@
 package nl.giantit.minecraft.GiantShop.core.Eco.Engines;
 
+import nl.giantit.minecraft.GiantShop.GiantShop;
+import nl.giantit.minecraft.GiantShop.core.Eco.iEco;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.PluginEnableEvent;
+import org.bukkit.event.server.PluginDisableEvent;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import java.util.logging.Level;
+
+import cosine.boseconomy.BOSEconomy;
 
 /**
  *
  * @author Giant
  */
 public class bose7_Engine implements iEco {
+	
+	private GiantShop plugin;
+	private BOSEconomy eco;
+	
+	public bose7_Engine(GiantShop plugin) {
+		this.plugin = plugin;
+		Bukkit.getServer().getPluginManager().registerEvents(new EcoListener(this), plugin);
+		if(eco == null) {
+			Plugin ecoEn = plugin.getServer().getPluginManager().getPlugin("BOSEconomy");
+
+			if(ecoEn != null && ecoEn.isEnabled()) {
+				eco = (BOSEconomy) ecoEn;
+				plugin.getLogger().log(Level.INFO, "[" + plugin.getPubName() + "] Succesfully hooked into BOSEconomy 7!");
+			}
+		}
+	}
 	
 	@Override
 	public boolean isLoaded() {
@@ -20,7 +49,7 @@ public class bose7_Engine implements iEco {
 	
 	@Override
 	public double getBalance(String player) {
-		return 0.0;
+		return eco.getPlayerMoneyDouble(player);
 	}
 	
 	@Override
@@ -30,6 +59,11 @@ public class bose7_Engine implements iEco {
 	
 	@Override
 	public boolean withdraw(String player, double amount) {
+		if(amount > 0) {
+			double balance = eco.getPlayerMoneyDouble(player);
+			return eco.setPlayerMoney(player, (balance - amount), true);
+		}
+		
 		return false;
 	}
 	
@@ -40,6 +74,40 @@ public class bose7_Engine implements iEco {
 	
 	@Override
 	public boolean deposit(String player, double amount) {
+		if(amount > 0) {
+			return eco.addPlayerMoney(player, amount, true);
+		}
+		
 		return false;
+	}
+	
+	public class EcoListener implements Listener {
+		private bose7_Engine eco;
+		
+		public EcoListener(bose7_Engine eco) {
+			this.eco = eco;
+		}
+		
+		@EventHandler()
+		public void onPluginEnable(PluginEnableEvent event) {
+			if(eco.eco == null) {
+				Plugin ecoEn = plugin.getServer().getPluginManager().getPlugin("BOSEconomy");
+				
+				if(ecoEn != null && ecoEn.isEnabled()) {
+					eco.eco = (BOSEconomy) ecoEn;
+					plugin.getLogger().log(Level.INFO, "[" + plugin.getPubName() + "] Succesfully hooked into BOSEconomy 7!");
+				}
+			}
+		}
+		
+		@EventHandler()
+		public void onPluginDisable(PluginDisableEvent event) {
+			if(eco.eco != null) {
+				if(event.getPlugin().getDescription().getName().equals("BOSEconomy")) {
+					eco.eco = null;
+					plugin.getLogger().log(Level.INFO, "[" + plugin.getPubName() + "] Succesfully unhooked into BOSEconomy 7!");
+				}
+			}
+		}
 	}
 }
