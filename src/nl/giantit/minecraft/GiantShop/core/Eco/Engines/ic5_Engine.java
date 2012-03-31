fb3095970d885584d2b6f338b5d4f4d4ce65a399
Giant <giant@giantit.nl>
2012-03-31 04:07:48 +0200

    This should do for the iConomy5 engine! :)

--- src/nl/giantit/minecraft/GiantShop/core/Eco/Engines/ic5_Engine.java
+++ src/nl/giantit/minecraft/GiantShop/core/Eco/Engines/ic5_Engine.java
@@ -1,6 +1,19 @@
 package nl.giantit.minecraft.GiantShop.core.Eco.Engines;
 
+import nl.giantit.minecraft.GiantShop.GiantShop;
+import nl.giantit.minecraft.GiantShop.core.Eco.iEco;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.PluginEnableEvent;
+import org.bukkit.event.server.PluginDisableEvent;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import java.util.logging.Level;
+
+import com.iConomy.iConomy;
 
 /**
  *
@@ -8,9 +21,25 @@ import org.bukkit.entity.Player;
  */
 public class ic5_Engine implements iEco {
 	
+	private GiantShop plugin;
+	private iConomy eco;
+	
+	public ic5_Engine(GiantShop plugin) {
+		this.plugin = plugin;
+		Bukkit.getServer().getPluginManager().registerEvents(new EcoListener(this), plugin);
+		if(eco == null) {
+			Plugin ecoEn = plugin.getServer().getPluginManager().getPlugin("iConomy");
+
+			if(ecoEn != null && ecoEn.isEnabled() && ecoEn.getClass().getName().equals("com.iConomy.iConomy")) {
+				eco = (iConomy) ecoEn;
+				plugin.getLogger().log(Level.INFO, "[" + plugin.getPubName() + "] Succesfully hooked into iConomy 5!");
+			}
+		}
+	}
+	
 	@Override
 	public boolean isLoaded() {
-		return false;
+		return eco != null;
 	}
 	
 	@Override
@@ -20,7 +49,7 @@ public class ic5_Engine implements iEco {
 	
 	@Override
 	public double getBalance(String player) {
-		return 0.0;
+		return eco.getAccount(player).getHoldings().balance();
 	}
 	
 	@Override
@@ -30,6 +59,15 @@ public class ic5_Engine implements iEco {
 	
 	@Override
 	public boolean withdraw(String player, double amount) {
+		if(amount > 0) {
+			double balance = eco.getAccount(player).getHoldings().balance();
+			amount = Math.ceil(amount);
+			if((balance - amount) >= 0) {
+				eco.getAccount(player).getHoldings().subtract(amount);
+				return true;
+			}
+		}
+		
 		return false;
 	}
 	
@@ -40,6 +78,40 @@ public class ic5_Engine implements iEco {
 	
 	@Override
 	public boolean deposit(String player, double amount) {
+		if(amount > 0) {
+			eco.getAccount(player).getHoldings().add(amount);
+			return true;
+		}
 		return false;
+	}
+	
+	public class EcoListener implements Listener {
+		private ic5_Engine eco;
+		
+		public EcoListener(ic5_Engine eco) {
+			this.eco = eco;
+		}
+		
+		@EventHandler()
+		public void onPluginEnable(PluginEnableEvent event) {
+			if(eco.eco == null) {
+				Plugin ecoEn = plugin.getServer().getPluginManager().getPlugin("iConomy");
+				
+				if(ecoEn != null && ecoEn.isEnabled() && ecoEn.getClass().getName().equals("com.iConomy.iConomy")) {
+					eco.eco = (iConomy) ecoEn;
+					plugin.getLogger().log(Level.INFO, "[" + plugin.getPubName() + "] Succesfully hooked into iConomy 5!");
+				}
+			}
+		}
+		
+		@EventHandler()
+		public void onPluginDisable(PluginDisableEvent event) {
+			if(eco.eco != null) {
+				if(event.getPlugin().getDescription().getName().equals("iConomy")) {
+					eco.eco = null;
+					plugin.getLogger().log(Level.INFO, "[" + plugin.getPubName() + "] Succesfully unhooked into iConomy 5!");
+				}
+			}
+		}
 	}
 }
