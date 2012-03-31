08d59f2480572b664245180fed814c8440c0f5e3
Giant <giant@giantit.nl>
2012-03-31 19:37:08 +0200

    Massive rewrite of Eco!

--- src/nl/giantit/minecraft/GiantShop/core/Eco/Eco.java
+++ src/nl/giantit/minecraft/GiantShop/core/Eco/Eco.java
@@ -1,6 +1,8 @@
 package nl.giantit.minecraft.GiantShop.core.Eco;
 
+import java.util.logging.Level;
 import nl.giantit.minecraft.GiantShop.GiantShop;
+import nl.giantit.minecraft.GiantShop.core.config;
 import nl.giantit.minecraft.GiantShop.core.Eco.Engines.*;
 
 /**
@@ -11,23 +13,25 @@ public class Eco {
 	
 	private iEco Engine = null;
 	private GiantShop plugin;
+	private config conf = config.Obtain();
+	Engines engine;
 	
 	public enum Engines {
-		AEco,
-		CraftConomy,
-		CurrencyCore,
-		EconXP,
-		Essentials,
-		McMoney,
-		MineConomy,
-		MultiCurrency,
+		AECO,
+		CRAFTCONOMY,
+		CURRENCYCORE,
+		ECONXP,
+		ESSENTIALS,
+		MCMONEY,
+		MINECONOMY,
+		MULTICURRENCY,
 		BOSE5,
 		BOSE6,
-		eWallet,
-		ic3o,
-		iConomy4,
-		iConomy5,
-		iConomy6
+		EWALLET,
+		IC3O,
+		ICONOMY4,
+		ICONOMY5,
+		ICONOMY6
 	}
 	
 	private boolean packageExists(String...Packages) {
@@ -41,54 +45,137 @@ public class Eco {
 		}
 	}
 	
+	private Engines findEngine(String engine) {
+		if(engine.equalsIgnoreCase("AECO")) {
+			return Engines.AECO;
+		}else if(engine.equalsIgnoreCase("CRAFTCONOMY")) {
+			return Engines.CRAFTCONOMY;
+		}else if(engine.equalsIgnoreCase("CURRENCYCORE")) {
+			return Engines.CURRENCYCORE;
+		}else if(engine.equalsIgnoreCase("ECONXP")) {
+			return Engines.ECONXP;
+		}else if(engine.equalsIgnoreCase("ESSENTIALS")) {
+			return Engines.ESSENTIALS;
+		}else if(engine.equalsIgnoreCase("MCMONEY")) {
+			return Engines.MCMONEY;
+		}else if(engine.equalsIgnoreCase("MINECONOMY")) {
+			return Engines.MINECONOMY;
+		}else if(engine.equalsIgnoreCase("MULTICURRENCY")) {
+			return Engines.MULTICURRENCY;
+		}else if(engine.equalsIgnoreCase("BOSE5")) {
+			return Engines.BOSE5;
+		}else if(engine.equalsIgnoreCase("BOSE6")) {
+			return Engines.BOSE6;
+		}else if(engine.equalsIgnoreCase("EWALLET")) {
+			return Engines.EWALLET;
+		}else if(engine.equalsIgnoreCase("IC3O")) {
+			return Engines.IC3O;
+		}else if(engine.equalsIgnoreCase("ICONOMY4")) {
+			return Engines.ICONOMY4;
+		}else if(engine.equalsIgnoreCase("ICONOMY5")) {
+			return Engines.ICONOMY5;
+		}else if(engine.equalsIgnoreCase("ICONOMY6")) {
+			return Engines.ICONOMY6;
+		}
+		
+		return null;
+	}
+	
 	public Eco(GiantShop plugin) {
 		this.plugin = plugin;
+		this.engine = this.findEngine(conf.getString("GiantShop.Economy.Engine"));
 		
-		if(packageExists("org.neocraft.AEco.AEco")) {
-			//AEco
-			Engine = new AEco_Engine(this.plugin);
-		}else if(packageExists("me.greatman.Craftconomy.Craftconomy")) {
-			//CraftConomy
-			
-		}else if(packageExists("is.currency.Currency")) {
-			//Currency
-			
-		}else if(packageExists("ca.agnate.EconXP.EconXP")) {
-			//EconXP
-			
-		}else if(packageExists("com.earth2me.essentials.api.Economy", "com.earth2me.essentials.api.NoLoanPermittedException", "com.earth2me.essentials.api.UserDoesNotExistException")) {
-			//Essentials
-			Engine = new Essentials_Engine(this.plugin);
-		}else if(packageExists("boardinggamer.mcmoney.McMoneyAPI")) {
-			//McMoney
-			
-		}else if(packageExists("me.mjolnir.mineconomy.MineConomy")) {
-			//MineConomy
-			
-		}else if(packageExists("me.ashtheking.currency.Currency", "me.ashtheking.currency.CurrencyList")) {
-			//MultiCurrency
-			
-		}else if(packageExists("cosine.boseconomy.BOSEconomy", "cosine.boseconomy.CommandManager")) {
-			//BOSE6
-			Engine = new bose6_Engine(this.plugin);
-		}else if(packageExists("cosine.boseconomy.BOSEconomy", "cosine.boseconomy.CommandHandler")) {
-			//BOSE7
-			Engine = new bose7_Engine(this.plugin);
-		}else if(packageExists("me.ethan.eWallet.ECO")) {
-			//eWallet
-			
-		}else if(packageExists("me.ic3d.eco.ECO")) {
-			//ic3o
-			
-		}else if(packageExists("com.nijiko.coelho.iConomy.iConomy", "com.nijiko.coelho.iConomy.system.Account")) {
-			//ic4
-			Engine = new ic4_Engine(this.plugin);
-		}else if(packageExists("com.iConomy.iConomy", "com.iConomy.system.Account", "com.iConomy.system.Holdings")) {
-			//ic5
-			Engine = new ic5_Engine(this.plugin);
-		}else if(packageExists("com.iCo6.iConomy")) {
-			//ic6
-			Engine = new ic6_Engine(this.plugin);
+		switch(this.engine) {
+			case AECO:
+				if(packageExists("org.neocraft.AEco.AEco")) {
+					//AEco
+					Engine = new AEco_Engine(this.plugin);
+				}
+				break;
+			case CRAFTCONOMY:
+				if(packageExists("me.greatman.Craftconomy.Craftconomy")) {
+					//CraftConomy
+					plugin.getLogger().log(Level.WARNING, "[" + plugin.getName() + "] CraftConomy is currently not yet supported!");
+				}
+				break;
+			case CURRENCYCORE:
+				if(packageExists("is.currency.Currency")) {
+					//CurrencyCore
+					plugin.getLogger().log(Level.WARNING, "[" + plugin.getName() + "] CurrencyCore is currently not yet supported!");
+				}
+				break;
+			case ECONXP:
+				if(packageExists("ca.agnate.EconXP.EconXP")) {
+					//EconXP
+					plugin.getLogger().log(Level.WARNING, "[" + plugin.getName() + "] EconXP is currently not yet supported!");
+				}
+				break;
+			case ESSENTIALS:
+				if(packageExists("com.earth2me.essentials.api.Economy", "com.earth2me.essentials.api.NoLoanPermittedException", "com.earth2me.essentials.api.UserDoesNotExistException")) {
+					//Essentials
+					Engine = new Essentials_Engine(this.plugin);
+				}
+				break;
+			case MCMONEY:
+				if(packageExists("boardinggamer.mcmoney.McMoneyAPI")) {
+					//McMoney
+					plugin.getLogger().log(Level.WARNING, "[" + plugin.getName() + "] McMoney is currently not yet supported!");
+				}
+				break;
+			case MINECONOMY:
+				if(packageExists("me.mjolnir.mineconomy.MineConomy")) {
+					//MineConomy
+					plugin.getLogger().log(Level.WARNING, "[" + plugin.getName() + "] MineConomy is currently not yet supported!");
+				} 
+				break;
+			case MULTICURRENCY:
+				if(packageExists("me.ashtheking.currency.Currency", "me.ashtheking.currency.CurrencyList")) {
+					//MultiCurrency
+					plugin.getLogger().log(Level.WARNING, "[" + plugin.getName() + "] MultiCurrency is currently not yet supported!");
+				}
+				break;
+			case BOSE5:
+				if(packageExists("cosine.boseconomy.BOSEconomy", "cosine.boseconomy.CommandManager")) {
+					//BOSE6
+					Engine = new bose6_Engine(this.plugin);
+				}
+				break;
+			case BOSE6:
+				if(packageExists("cosine.boseconomy.BOSEconomy", "cosine.boseconomy.CommandHandler")) {
+					//BOSE7
+					Engine = new bose7_Engine(this.plugin);
+				}
+				break;
+			case EWALLET:
+				if(packageExists("me.ethan.eWallet.ECO")) {
+					//eWallet
+					plugin.getLogger().log(Level.WARNING, "[" + plugin.getName() + "] eWallet is currently not yet supported!");
+				}
+				break;
+			case IC3O:
+				if(packageExists("me.ic3d.eco.ECO")) {
+					//ic3o
+					plugin.getLogger().log(Level.WARNING, "[" + plugin.getName() + "] 3co is currently not yet supported!");
+				}
+				break;
+			case ICONOMY4:
+				if(packageExists("com.nijiko.coelho.iConomy.iConomy", "com.nijiko.coelho.iConomy.system.Account")) {
+					//ic4
+					Engine = new ic4_Engine(this.plugin);
+				}
+				break;
+			case ICONOMY5:
+				if(packageExists("com.iConomy.iConomy", "com.iConomy.system.Account", "com.iConomy.system.Holdings")) {
+					//ic5
+					Engine = new ic5_Engine(this.plugin);
+				}
+				break;
+			case ICONOMY6:
+				if(packageExists("com.iCo6.iConomy")) {
+					//ic6
+					Engine = new ic6_Engine(this.plugin);
+				}
+				break;
 		}
 	}
 	
