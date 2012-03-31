3b30eb3c160a31dc4c03589f2808f8377839debb
Giant <giant@giantit.nl>
2012-03-31 04:00:29 +0200

    That should do

--- src/nl/giantit/minecraft/GiantShop/core/Eco/Engines/ic4_Engine.java
+++ src/nl/giantit/minecraft/GiantShop/core/Eco/Engines/ic4_Engine.java
@@ -1,5 +1,6 @@
 package nl.giantit.minecraft.GiantShop.core.Eco.Engines;
 
+import nl.giantit.minecraft.GiantShop.GiantShop;
 import nl.giantit.minecraft.GiantShop.core.Eco.iEco;
 
 import org.bukkit.Bukkit;
@@ -21,9 +22,25 @@ import com.nijiko.coelho.iConomy.system.Account;
  */
 public class ic4_Engine implements iEco {
 	
+	private GiantShop plugin;
+	private iConomy eco;
+	
+	public ic4_Engine(GiantShop plugin) {
+		this.plugin = plugin;
+		Bukkit.getServer().getPluginManager().registerEvents(new EcoListener(this), plugin);
+		if(eco == null) {
+			Plugin ecoEn = plugin.getServer().getPluginManager().getPlugin("iConomy");
+
+			if(ecoEn != null && ecoEn.isEnabled()) {
+				eco = (iConomy) ecoEn;
+				plugin.getLogger().log(Level.INFO, "[" + plugin.getPubName() + "] Succesfully hooked into iConomy 4!");
+			}
+		}
+	}
+	
 	@Override
 	public boolean isLoaded() {
-		return false;
+		return eco != null;
 	}
 	
 	@Override
@@ -33,7 +50,13 @@ public class ic4_Engine implements iEco {
 	
 	@Override
 	public double getBalance(String player) {
-		return 0.0;
+		Account account = eco.getBank().getAccount(player);
+		if(account == null) {
+			eco.getBank().addAccount(player);
+			account = eco.getBank().getAccount(player);
+		}
+		
+		return account.getBalance();
 	}
 	
 	@Override
@@ -43,6 +66,21 @@ public class ic4_Engine implements iEco {
 	
 	@Override
 	public boolean withdraw(String player, double amount) {
+		if(amount > 0) {
+			Account account = eco.getBank().getAccount(player);
+			if(account == null) {
+				eco.getBank().addAccount(player);
+				account = eco.getBank().getAccount(player);
+			}
+			
+			double balance = account.getBalance();
+			amount = Math.ceil(amount);
+			if((balance - amount) >= 0) {
+				account.subtract(amount);
+				return true;
+			}
+		}
+		
 		return false;
 	}
 	
@@ -53,6 +91,45 @@ public class ic4_Engine implements iEco {
 	
 	@Override
 	public boolean deposit(String player, double amount) {
+		if(amount > 0) {
+			Account account = eco.getBank().getAccount(player);
+			if(account == null) {
+				eco.getBank().addAccount(player);
+				account = eco.getBank().getAccount(player);
+			}
+			
+			account.add(amount);
+		}
 		return false;
+	}
+	
+	public class EcoListener implements Listener {
+		private ic4_Engine eco;
+		
+		public EcoListener(ic4_Engine eco) {
+			this.eco = eco;
+		}
+		
+		@EventHandler()
+		public void onPluginEnable(PluginEnableEvent event) {
+			if(eco.eco == null) {
+				Plugin ecoEn = plugin.getServer().getPluginManager().getPlugin("iConomy");
+				
+				if(ecoEn != null && ecoEn.isEnabled()) {
+					eco.eco = (iConomy) ecoEn;
+					plugin.getLogger().log(Level.INFO, "[" + plugin.getPubName() + "] Succesfully hooked into iConomy 4!");
+				}
+			}
+		}
+		
+		@EventHandler()
+		public void onPluginDisable(PluginDisableEvent event) {
+			if(eco.eco != null) {
+				if(event.getPlugin().getDescription().getName().equals("iConomy")) {
+					eco.eco = null;
+					plugin.getLogger().log(Level.INFO, "[" + plugin.getPubName() + "] Succesfully unhooked into iConomy 4!");
+				}
+			}
+		}
 	}
 }
