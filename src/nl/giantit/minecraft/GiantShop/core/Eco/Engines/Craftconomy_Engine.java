791f743649fffa52a73613b1911b5983dec0a738
Giant <giant@giantit.nl>
2012-04-02 18:49:15 +0200

    Include support for both Craftconomy and CurrencyCore

--- src/nl/giantit/minecraft/GiantShop/core/Eco/Engines/Craftconomy_Engine.java
+++ src/nl/giantit/minecraft/GiantShop/core/Eco/Engines/Craftconomy_Engine.java
@@ -1,12 +1,48 @@
 package nl.giantit.minecraft.GiantShop.core.Eco.Engines;
 
+import nl.giantit.minecraft.GiantShop.GiantShop;
+import nl.giantit.minecraft.GiantShop.core.Eco.iEco;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.PluginEnableEvent;
+import org.bukkit.event.server.PluginDisableEvent;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import java.util.logging.Level;
+
+import me.greatman.Craftconomy.Craftconomy;
+import me.greatman.Craftconomy.Account;
+import me.greatman.Craftconomy.AccountHandler;
 
 /**
  *
  * @author Giant
  */
 public class Craftconomy_Engine implements iEco {
+	
+	private GiantShop plugin;
+	private Craftconomy eco = null;
+	
+	public Craftconomy_Engine(GiantShop plugin) {
+		this.plugin = plugin;
+		Bukkit.getServer().getPluginManager().registerEvents(new EcoListener(this), plugin);
+		
+		if(eco == null) {
+			Plugin ecoEn = plugin.getServer().getPluginManager().getPlugin("Craftconomy");
+
+			if(ecoEn != null && ecoEn.isEnabled()) {
+				eco = (Craftconomy) ecoEn;
+				if(eco == null) {
+					plugin.getLogger().log(Level.WARNING, "Failed to hook into Craftconomy!");
+				}else{
+					plugin.getLogger().log(Level.INFO, "Succesfully hooked into Craftconomy!");
+				}
+			}
+		}
+	}
 	
 	@Override
 	public boolean isLoaded() {
@@ -20,7 +56,8 @@ public class Craftconomy_Engine implements iEco {
 	
 	@Override
 	public double getBalance(String player) {
-		return 0.0;
+		Account acc = AccountHandler.getAccount(player);
+		return acc.getDefaultBalance();
 	}
 	
 	@Override
@@ -30,6 +67,12 @@ public class Craftconomy_Engine implements iEco {
 	
 	@Override
 	public boolean withdraw(String player, double amount) {
+		Account acc = AccountHandler.getAccount(player);
+		if(acc.hasEnough(amount)) {
+			acc.substractMoney(amount);
+			
+			return true;
+		}
 		return false;
 	}
 	
@@ -40,6 +83,39 @@ public class Craftconomy_Engine implements iEco {
 	
 	@Override
 	public boolean deposit(String player, double amount) {
-		return false;
+		Account acc = AccountHandler.getAccount(player);
+		acc.addMoney(amount);
+		
+		return true;
+	}
+	
+	public class EcoListener implements Listener {
+		private Craftconomy_Engine eco;
+		
+		public EcoListener(Craftconomy_Engine eco) {
+			this.eco = eco;
+		}
+		
+		@EventHandler()
+		public void onPluginEnable(PluginEnableEvent event) {
+			if(eco.eco == null) {
+				Plugin ecoEn = plugin.getServer().getPluginManager().getPlugin("Craftconomy");
+				
+				if(ecoEn != null && ecoEn.isEnabled()) {
+					eco.eco = (Craftconomy) ecoEn;
+					plugin.getLogger().log(Level.INFO, "Succesfully hooked into Craftconomy!");
+				}
+			}
+		}
+		
+		@EventHandler()
+		public void onPluginDisable(PluginDisableEvent event) {
+			if(eco.eco != null) {
+				if(event.getPlugin().getDescription().getName().equals("Craftconomy")) {
+					eco.eco = null;
+					plugin.getLogger().log(Level.INFO, "Succesfully unhooked into Craftconomy!");
+				}
+			}
+		}
 	}
 }
--- src/nl/giantit/minecraft/GiantShop/core/Eco/Engines/CurrencyCore_Engine.java
+++ src/nl/giantit/minecraft/GiantShop/core/Eco/Engines/CurrencyCore_Engine.java
@@ -1,12 +1,47 @@
 package nl.giantit.minecraft.GiantShop.core.Eco.Engines;
 
+import nl.giantit.minecraft.GiantShop.GiantShop;
+import nl.giantit.minecraft.GiantShop.core.Eco.iEco;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.PluginEnableEvent;
+import org.bukkit.event.server.PluginDisableEvent;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import java.util.logging.Level;
+
+import is.currency.Currency;
+import is.currency.syst.AccountContext;
 
 /**
  *
  * @author Giant
  */
 public class CurrencyCore_Engine implements iEco {
+	
+	private GiantShop plugin;
+	private Currency eco = null;
+	
+	public CurrencyCore_Engine(GiantShop plugin) {
+		this.plugin = plugin;
+		Bukkit.getServer().getPluginManager().registerEvents(new EcoListener(this), plugin);
+		
+		if(eco == null) {
+			Plugin ecoEn = plugin.getServer().getPluginManager().getPlugin("CurrencyCore");
+
+			if(ecoEn != null && ecoEn.isEnabled()) {
+				eco = (Currency) ecoEn;
+				if(eco == null) {
+					plugin.getLogger().log(Level.WARNING, "Failed to hook into CurrencyCore!");
+				}else{
+					plugin.getLogger().log(Level.INFO, "Succesfully hooked into CurrencyCore!");
+				}
+			}
+		}
+	}
 	
 	@Override
 	public boolean isLoaded() {
@@ -20,7 +55,11 @@ public class CurrencyCore_Engine implements iEco {
 	
 	@Override
 	public double getBalance(String player) {
-		return 0.0;
+		AccountContext acc = eco.getAccountManager().getAccount(player);
+		if(acc == null)
+			return 0.0;
+		
+		return acc.getBalance();
 	}
 	
 	@Override
@@ -30,6 +69,14 @@ public class CurrencyCore_Engine implements iEco {
 	
 	@Override
 	public boolean withdraw(String player, double amount) {
+		AccountContext acc = eco.getAccountManager().getAccount(player);
+		if(acc != null) {
+			if(acc.hasBalance(amount)) {
+				acc.subtractBalance(amount);
+				return true;
+			}
+		}
+		
 		return false;
 	}
 	
@@ -40,6 +87,42 @@ public class CurrencyCore_Engine implements iEco {
 	
 	@Override
 	public boolean deposit(String player, double amount) {
+		AccountContext acc = eco.getAccountManager().getAccount(player);
+		if(acc != null) {
+			acc.addBalance(amount);
+			return true;
+		}
+		
 		return false;
+	}
+	
+	public class EcoListener implements Listener {
+		private CurrencyCore_Engine eco;
+		
+		public EcoListener(CurrencyCore_Engine eco) {
+			this.eco = eco;
+		}
+		
+		@EventHandler()
+		public void onPluginEnable(PluginEnableEvent event) {
+			if(eco.eco == null) {
+				Plugin ecoEn = plugin.getServer().getPluginManager().getPlugin("CurrencyCore");
+				
+				if(ecoEn != null && ecoEn.isEnabled()) {
+					eco.eco = (Currency) ecoEn;
+					plugin.getLogger().log(Level.INFO, "Succesfully hooked into CurrencyCore!");
+				}
+			}
+		}
+		
+		@EventHandler()
+		public void onPluginDisable(PluginDisableEvent event) {
+			if(eco.eco != null) {
+				if(event.getPlugin().getDescription().getName().equals("CurrencyCore")) {
+					eco.eco = null;
+					plugin.getLogger().log(Level.INFO, "Succesfully unhooked into CurrencyCore!");
+				}
+			}
+		}
 	}
 }
