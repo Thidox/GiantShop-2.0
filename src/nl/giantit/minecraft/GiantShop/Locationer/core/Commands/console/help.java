307e963efdeae73c9de5d7838c47b960c8b8ffe8
Giant <giant@tpre.nl>
2012-04-06 01:24:17 +0200

    Include console version of help command too

---
+++ src/nl/giantit/minecraft/GiantShop/Locationer/core/Commands/console/help.java
@@ -0,0 +1,14 @@
+package nl.giantit.minecraft.GiantShop.Locationer.core.Commands.console;
+
+import org.bukkit.command.CommandSender;
+
+/**
+ *
+ * @author Giant
+ */
+public class help {
+	
+	public static void showHelp(CommandSender sender, String[] args) {
+		
+	}
+}
