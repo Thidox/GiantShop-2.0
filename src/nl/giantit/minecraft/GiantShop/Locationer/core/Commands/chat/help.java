652b67f3178a8303194f2b6d325939eaee7aedc4
Giant <giant@tpre.nl>
2012-04-06 01:23:40 +0200

    Changed command structure for locationer to be more clean

---
+++ src/nl/giantit/minecraft/GiantShop/Locationer/core/Commands/chat/help.java
@@ -0,0 +1,14 @@
+package nl.giantit.minecraft.GiantShop.Locationer.core.Commands.chat;
+
+import org.bukkit.entity.Player;
+
+/**
+ *
+ * @author Giant
+ */
+public class help {
+	
+	public static void showHelp(Player player, String[] args) {
+		
+	}
+}
