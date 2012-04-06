95405f6f5c5e016629038071bd4cc047fb58c7e1
Giant <giant@tpre.nl>
2012-04-06 05:01:53 +0200

    Start work on location add command

---
+++ src/nl/giantit/minecraft/GiantShop/Locationer/core/Commands/chat/add.java
@@ -0,0 +1,34 @@
+package nl.giantit.minecraft.GiantShop.Locationer.core.Commands.chat;
+
+import nl.giantit.minecraft.GiantShop.GiantShop;
+import nl.giantit.minecraft.GiantShop.Misc.Heraut;
+import nl.giantit.minecraft.GiantShop.Misc.Messages;
+import nl.giantit.minecraft.GiantShop.core.config;
+import nl.giantit.minecraft.GiantShop.core.perm;
+
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Giant
+ */
+public class add {
+	
+	private static config conf = config.Obtain();
+	private static perm perms = perm.Obtain();
+	private static Messages mH = GiantShop.getPlugin().getMsgHandler();
+	
+	public static void add(Player player, String[] args) {
+		if(perms.has(player, "giantshop.location.add")) {
+			String name = (args.length > 1) ? args[1] : "unkown";
+		}else{
+			HashMap<String, String> data = new HashMap<String, String>();
+			data.put("command", "loc add");
+
+			Heraut.say(mH.getMsg(Messages.msgType.ERROR, "noPermissions", data));
+		}
+	}
+	
+}
