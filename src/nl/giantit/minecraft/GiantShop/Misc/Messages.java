d40c97ff0afffca41cfc9f0d400a2a393755d2c1
Giant <giant@giantit.nl>
2012-04-18 00:02:55 +0200

    Made a couple slight changes to the msgTemplate

--- src/nl/giantit/minecraft/GiantShop/Misc/Messages.java
+++ src/nl/giantit/minecraft/GiantShop/Misc/Messages.java
@@ -18,7 +18,7 @@ public class Messages {
 	
 	private GiantShop plugin;
 	private YamlConfiguration config;
-	private double yamlVersion = 1.0;
+	private double yamlVersion = 1.1;
 	private HashMap<String, String> mainMsgs, adminMsgs, errorMsgs;
 	
 	public enum msgType {
