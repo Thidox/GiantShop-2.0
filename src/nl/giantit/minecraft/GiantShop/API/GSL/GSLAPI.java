e8100576fe3524c2325841383385b1edaef47cc5
Giant <giant@tpre.nl>
2012-05-10 19:51:02 +0200

    Add location handler to the GSL API

--- src/nl/giantit/minecraft/GiantShop/API/GSL/GSLAPI.java
+++ src/nl/giantit/minecraft/GiantShop/API/GSL/GSLAPI.java
@@ -1,14 +1,19 @@
 package nl.giantit.minecraft.GiantShop.API.GSL;
 
 import nl.giantit.minecraft.GiantShop.GiantShop;
+import nl.giantit.minecraft.GiantShop.Locationer.Locationer;
 import nl.giantit.minecraft.GiantShop.core.Database.db;
 
 public class GSLAPI {
 
 	private GiantShop plugin;
+	private Locationer loc;
 	
 	public GSLAPI(GiantShop plugin) {
 		this.plugin = plugin;
+		this.loc = plugin.getLocHandler();
 	}
+	
+	
 	
 }
