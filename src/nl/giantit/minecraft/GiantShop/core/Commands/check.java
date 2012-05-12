7221d8ba40cc47fb54be8d9e2b6d36eb135a9cd5
Giant <giant@tpre.nl>
2012-05-12 21:34:02 +0200

    Include all the shiny new things into the console version of check

--- src/nl/giantit/minecraft/GiantShop/core/Commands/check.java
+++ src/nl/giantit/minecraft/GiantShop/core/Commands/check.java
@@ -237,27 +237,80 @@ public class check {
 		fields.add("sellFor");
 		fields.add("buyFor");
 		fields.add("stock");
+		fields.add("maxStock");
 		fields.add("shops");
-
+		
 		HashMap<String, String> where = new HashMap<String, String>();
 		where.put("itemID", String.valueOf(itemID));
 		where.put("type", String.valueOf(itemType));
-
+		
 		ArrayList<HashMap<String, String>> resSet = DB.select(fields).from("#__items").where(where).execQuery();
 		if(resSet.size() == 1) {
 			//Wait didn't we just do this the other way round?!
 			//Yea we did! Why? Because we can!
 			itemType = (itemType == -1) ? 0 : itemType;
-
+			
 			String name = iH.getItemNameByID(itemID, itemType);
 			HashMap<String, String> res = resSet.get(0);
+			
+			int stock = Integer.parseInt(res.get("stock"));
+			int maxStock = Integer.parseInt(res.get("maxStock"));
+			double sellFor = Double.parseDouble(res.get("sellFor"));
+			double buyFor = Double.parseDouble(res.get("buyFor"));
+			
+			if(conf.getBoolean("GiantShop.stock.useStock") && conf.getBoolean("GiantShop.stock.stockDefinesCost") && maxStock != -1 && stock != -1) {
+				double maxInfl = conf.getDouble("GiantShop.stock.maxInflation");
+				double maxDefl = conf.getDouble("GiantShop.stock.maxDeflation");
+				int atmi = conf.getInt("GiantShop.stock.amountTillMaxInflation");
+				int atmd = conf.getInt("GiantShop.stock.amountTillMaxDeflation");
+				double split = Math.round((atmi + atmd) / 2);
+				if(maxStock <= atmi + atmd); {
+					split = maxStock / 2;
+					atmi = 0;
+					atmd = maxStock;
+				}
+				
+				if(stock >= atmd) {
+					if(buyFor != -1)
+						buyFor = buyFor * (1.0 - maxDefl / 100.0);
+					
+					if(sellFor != -1)
+						sellFor = sellFor * (1.0 - maxDefl / 100.0); 
+				}else if(stock <= atmi) {
+					if(buyFor != -1)
+						buyFor = buyFor * (1.0 + maxDefl / 100.0);
+					
+					if(sellFor != -1)
+						sellFor = sellFor * (1.0 + maxDefl / 100.0);
+				}else{
+					if(stock < split) {
+						if(buyFor != -1)
+							buyFor = (double)Math.round((buyFor * (1.0 + (maxInfl / stock) / 100)) * 100.0) / 100.0;
+
+						if(sellFor != -1)
+							sellFor = (double)Math.round((sellFor * (1.0 + (maxInfl / stock) / 100)) * 100.0) / 100.0;
+					}else if(stock > split) {
+						if(buyFor != -1)
+							buyFor = 2.0 + (double)Math.round((buyFor / (maxDefl * stock / 100)) * 100.0) / 100.0;
+
+						if(sellFor != -1)
+							sellFor = 2.0 + (double)Math.round((sellFor / (maxDefl * stock / 100)) * 100.0) / 100.0;
+						
+					}
+				}
+			}
+			
+			String sf = String.valueOf(sellFor);
+			String bf = String.valueOf(buyFor);
+
 			Heraut.say(sender, "Here's the result for " + name + "!");
 			Heraut.say(sender, "ID: " + itemID);
 			Heraut.say(sender, "Type: " + itemType);
 			Heraut.say(sender, "Quantity per amount: " + res.get("perStack"));
-			Heraut.say(sender, "Leaves shop for: " + (!res.get("sellFor").equals("-1.0") ? res.get("sellFor") : "Doesn't leave the shop!"));
-			Heraut.say(sender, "Returns to shop for: " + (!res.get("buyFor").equals("-1.0") ? res.get("buyFor") : "No returns!"));
+			Heraut.say(sender, "Leaves shop for: " + (!sf.equals("-1.0") ? sf : "Not for sale!"));
+			Heraut.say(sender, "Returns to shop for: " + (!bf.equals("-1.0") ? bf : "No returns!"));
 			Heraut.say(sender, "Amount of items in the shop: " + (!res.get("stock").equals("-1") ? res.get("stock") : "unlimited"));
+			Heraut.say(sender, "Maximum amount of items in the shop: " + (!res.get("maxStock").equals("-1") ? res.get("maxStock") : "unlimited"));
 			//More future stuff
 			/*if(conf.getBoolean("GiantShop.Location.useGiantShopLocation") == true) {
 			 *		ArrayList<Indaface> shops = GiantShop.getPlugin().getLocationHandler().parseShops(res.get("shops"));
