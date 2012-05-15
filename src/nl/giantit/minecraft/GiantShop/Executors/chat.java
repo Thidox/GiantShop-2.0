5df2e2316eb40bb2d6712f3da0a14f78de5ffe0c
Giant <giant@tpre.nl>
2012-05-15 17:23:18 +0200

    Fix incorrect calls to private field...

--- src/nl/giantit/minecraft/GiantShop/Executors/chat.java
+++ src/nl/giantit/minecraft/GiantShop/Executors/chat.java
@@ -25,25 +25,25 @@ public class chat {
 		Player player = (Player) sender;
 		
 		if(args.length == 0 || args[0].equalsIgnoreCase("help") || args[0].equalsIgnoreCase("h") || args[0].equalsIgnoreCase("?")) {
-			if(plugin.useLoc && plugin.cmds.contains("help")) {
+			if(plugin.useLocation() && plugin.cmds.contains("help")) {
 				if(plugin.getLocHandler().canUse(player))
 					help.showHelp(player, args);
 			}else
 				help.showHelp(player, args);
 		}else if(Misc.isEitherIgnoreCase(args[0], "sendhelp", "sh")) {
-			if(plugin.useLoc && plugin.cmds.contains("sendhelp")) {
+			if(plugin.useLocation() && plugin.cmds.contains("sendhelp")) {
 				if(plugin.getLocHandler().canUse(player))
 					help.sendHelp(player, args);
 			}else
 				help.sendHelp(player, args);
 		}else if(Misc.isEitherIgnoreCase(args[0], "list", "l")) {
-			if(plugin.useLoc && plugin.cmds.contains("list")) {
+			if(plugin.useLocation() && plugin.cmds.contains("list")) {
 				if(plugin.getLocHandler().canUse(player))
 					list.list(player, args);
 			}else
 				list.list(player, args);
 		}else if(Misc.isEitherIgnoreCase(args[0], "check", "c")) {
-			if(plugin.useLoc && plugin.cmds.contains("check")) {
+			if(plugin.useLocation() && plugin.cmds.contains("check")) {
 				if(plugin.getLocHandler().canUse(player))
 					check.check(player, args);
 			}else
@@ -51,37 +51,37 @@ public class chat {
 		}else if(args[0].equalsIgnoreCase("search")) {
 			//futuristic ideas! :D
 		}else if(Misc.isEitherIgnoreCase(args[0], "buy", "b")) {
-			if(plugin.useLoc && plugin.cmds.contains("buy")) {
+			if(plugin.useLocation() && plugin.cmds.contains("buy")) {
 				if(plugin.getLocHandler().canUse(player))
 					buy.buy(player, args);
 			}else
 				buy.buy(player, args);
 		}else if(Misc.isEitherIgnoreCase(args[0], "sell", "s")) {
-			if(plugin.useLoc && plugin.cmds.contains("sell")) {
+			if(plugin.useLocation() && plugin.cmds.contains("sell")) {
 				if(plugin.getLocHandler().canUse(player))
 					sell.sell(player, args);
 			}else
 				sell.sell(player, args);
 		}else if(Misc.isEitherIgnoreCase(args[0], "gift", "g")) {
-			if(plugin.useLoc && plugin.cmds.contains("gift")) {
+			if(plugin.useLocation() && plugin.cmds.contains("gift")) {
 				if(plugin.getLocHandler().canUse(player))
 					buy.gift(player, args);
 			}else
 				buy.gift(player, args);
 		}else if(Misc.isEitherIgnoreCase(args[0], "add", "a")) {
-			if(plugin.useLoc && plugin.cmds.contains("add")) {
+			if(plugin.useLocation() && plugin.cmds.contains("add")) {
 				if(plugin.getLocHandler().canUse(player))
 					add.add(player, args);
 			}else
 				add.add(player, args);
 		}else if(Misc.isEitherIgnoreCase(args[0], "update", "u")) {
-			if(plugin.useLoc && plugin.cmds.contains("update")) {
+			if(plugin.useLocation() && plugin.cmds.contains("update")) {
 				if(plugin.getLocHandler().canUse(player))
 					update.update(player, args);
 			}else
 				update.update(player, args);
 		}else if(Misc.isEitherIgnoreCase(args[0], "remove", "r")) {
-			if(plugin.useLoc && plugin.cmds.contains("remove")) {
+			if(plugin.useLocation() && plugin.cmds.contains("remove")) {
 				if(plugin.getLocHandler().canUse(player))
 					remove.remove(player, args);
 			}else
@@ -89,7 +89,7 @@ public class chat {
 		}else if(Misc.isEitherIgnoreCase(args[0], "addstock", "as")) {
 			//stalled
 			//Probably no longer required
-			if(plugin.useLoc && plugin.cmds.contains("addstock")) {
+			if(plugin.useLocation() && plugin.cmds.contains("addstock")) {
 				if(plugin.getLocHandler().canUse(player))
 					help.showHelp(player, args);
 			}else
