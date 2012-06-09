469c050fd2b6f19a99c7fbe500e60be47bc208d5
Giant <giant@tpre.nl>
2012-06-09 21:51:51 +0200

    Make it possible to show a new version message to ops when they log in

---
+++ src/nl/giantit/minecraft/GiantShop/Listeners/PlayerListener.java
@@ -0,0 +1,41 @@
+package nl.giantit.minecraft.GiantShop.Listeners;
+
+import nl.giantit.minecraft.GiantShop.GiantShop;
+import nl.giantit.minecraft.GiantShop.core.config;
+import nl.giantit.minecraft.GiantShop.Misc.Heraut;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;;
+
+public class PlayerListener implements Listener {
+	
+	private GiantShop plugin;
+	private config conf = config.Obtain();
+	
+	public PlayerListener(GiantShop plugin) {
+		this.plugin = plugin;
+	}
+	
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onPlayerJoin(final PlayerJoinEvent e) {
+		if(e.getPlayer().isOp()) {
+			if(plugin.isOutOfDate() && conf.getBoolean("GiantShop.Updater.broadcast")) {
+				plugin.scheduleAsyncDelayedTask(new Runnable() {
+					@Override
+					public void run()
+					{
+						delayedJoin(e.getPlayer());
+					}
+				});
+			}
+		}
+	}
+	
+	public void delayedJoin(Player p) {
+		Heraut.say(p.getPlayer(), "&cA new version of GiantShop has been released! You are currently running: " + plugin.getDescription().getVersion() + " while the latest version is: " + plugin.getNewVersion());
+	}
+
+}
