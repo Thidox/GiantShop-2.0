b3c8aeced5300d061bed5a507f214f6824affcfe
Giant <giant@tpre.nl>
2012-06-24 01:48:56 +0200

    Include start of Metrics system into repo.

---
+++ src/nl/giantit/minecraft/GiantShop/core/Metrics/MetricsHandler.java
@@ -0,0 +1,35 @@
+package nl.giantit.minecraft.GiantShop.core.Metrics;
+
+import nl.giantit.minecraft.GiantShop.GiantShop;
+import nl.giantit.minecraft.GiantShop.core.config;
+import nl.giantit.minecraft.GiantShop.core.Metrics.Metrics.Graph;
+
+import java.io.IOException;
+
+public class MetricsHandler {
+
+	public MetricsHandler(GiantShop plugin) {
+		config conf = config.Obtain();
+	    
+		try {
+			Metrics metrics = new Metrics(plugin);
+			
+			if(conf.getBoolean(plugin.getName() + ".metrics.send.database")) {
+				Graph graph = metrics.createGraph("Database Engine");
+				graph.addPlotter(new Metrics.Plotter() {
+					
+					@Override
+					public int getValue() {
+						return 1;
+					}
+				});
+			}
+			metrics.start();
+		} catch (IOException e) {
+			plugin.getLogger().warning("Failed to load metrics!");
+		    if(conf.getBoolean(plugin.getName() + ".global.debug")) {
+		    	e.printStackTrace();
+		    }
+		}
+	}
+}
