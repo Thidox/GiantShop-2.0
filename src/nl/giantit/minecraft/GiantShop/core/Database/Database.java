10eb624c681e069edd8f22fc9eae77eb62935bb0
Giant <giant@tpre.nl>
2012-06-24 02:24:52 +0200

    Add ability to get the main database type from Database.

--- src/nl/giantit/minecraft/GiantShop/core/Database/Database.java
+++ src/nl/giantit/minecraft/GiantShop/core/Database/Database.java
@@ -11,23 +11,46 @@ import java.util.HashMap;
  */
 public class Database {
 	
+	private enum dbType {
+		MySQL("MySQL"),
+		SQLite("SQLite");
+		
+		String value;
+		
+		private dbType(String s) {
+			this.value = s;
+		}
+		
+		@Override
+		public String toString() {
+			return this.value;
+		}
+	}
+	
 	private static HashMap<String, Database> instance = new HashMap<String, Database>();
 	private GiantShop plugin;
 	private iDriver dbDriver;
+	private dbType t;
 	
 	private Database(HashMap<String, String> conf, String instance) {
 		if(instance == null)
 			instance = "0";
 		
 		if(conf.get("driver").equalsIgnoreCase("MySQL")) {
+			t = dbType.MySQL;
 			this.dbDriver = MySQL.Obtain(conf, instance);
 		}else{
+			t = dbType.SQLite;
 			this.dbDriver = SQLite.Obtain(conf, instance);
 		}
 	}
 	
 	public iDriver getEngine() {
 		return this.dbDriver;
+	}
+	
+	public String getType() {
+		return t.toString();
 	}
 	
 	public static Database Obtain() {
