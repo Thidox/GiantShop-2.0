717719fc2104b77c0d29426d45cc0f5b1a1fb9c3
Giant <giant@tpre.nl>
2012-06-27 02:54:19 +0200

    Start work on discount commands.

---
+++ src/nl/giantit/minecraft/GiantShop/core/Commands/chat/Discount/Add.java
@@ -0,0 +1,17 @@
+package nl.giantit.minecraft.GiantShop.core.Commands.chat.Discount;
+
+import nl.giantit.minecraft.GiantShop.GiantShop;
+import nl.giantit.minecraft.GiantShop.Misc.Messages;
+import nl.giantit.minecraft.GiantShop.core.perms.Permission;
+
+import org.bukkit.entity.Player;
+
+public class Add {
+
+	private static Permission perms = GiantShop.getPlugin().getPermHandler().getEngine();
+	private static Messages mH = GiantShop.getPlugin().getMsgHandler();
+	
+	public static void exec(Player player, String[] args) {
+		
+	}
+}
