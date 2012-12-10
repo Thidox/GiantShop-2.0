069672c87c9a00c526e5efed0e5c25057a29ac11
Giant <giant@tpre.nl>
2012-12-10 04:44:21 +0100

    Fix those damned Â's again. Remove deprecated methods after no longer using them.

--- src/nl/giantit/minecraft/GiantShop/Misc/Heraut.java
+++ src/nl/giantit/minecraft/GiantShop/Misc/Heraut.java
@@ -1,6 +1,7 @@
 package nl.giantit.minecraft.GiantShop.Misc;
 
 import nl.giantit.minecraft.GiantShop.GiantShop;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.command.CommandSender;
 
@@ -10,22 +11,10 @@ import org.bukkit.command.CommandSender;
  * @author Giant
  */
 public class Heraut {
-	 
-	private static Player player = null;
-
-	@Deprecated
-	public static void savePlayer(Player player) {
-		Heraut.player = player;
-	}
 	
     public static String parse(String input) {
-        return input.replaceAll("(&([a-fA-F0-9]))", "§$2").replace("\\\\\u00A7", "&");
+        return ChatColor.translateAlternateColorCodes('&', input);//input.replaceAll("(&([a-fA-F0-9]))", "§$2").replace("\\\\\u00A7", "&");
     }
-
-	@Deprecated
-	public static void say (String message) {
-		player.sendMessage(parse(message));
-	}
 
 	public static void say (Player player, String message) {
 		player.sendMessage(parse(message));
