0ecc5d60be95510e9535967b12b2996bcb4097ea
Giant <giant@tpre.nl>
2012-12-10 04:45:49 +0100

    Finish up the GiantShopWebAPI trusted app list command.

--- src/nl/giantit/minecraft/GiantShop/API/GSW/Commands/Chat/List.java
+++ src/nl/giantit/minecraft/GiantShop/API/GSW/Commands/Chat/List.java
@@ -1,11 +1,14 @@
 package nl.giantit.minecraft.GiantShop.API.GSW.Commands.Chat;
 
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 import nl.giantit.minecraft.GiantShop.API.GSW.GSWAPI;
+import nl.giantit.minecraft.GiantShop.API.GSW.Server.ShopSender;
 import nl.giantit.minecraft.GiantShop.API.GiantShopAPI;
 import nl.giantit.minecraft.GiantShop.GiantShop;
 import nl.giantit.minecraft.GiantShop.Misc.Heraut;
 import nl.giantit.minecraft.GiantShop.Misc.Messages;
+import nl.giantit.minecraft.GiantShop.core.config;
 import nl.giantit.minecraft.GiantShop.core.perms.Permission;
 import org.bukkit.entity.Player;
 
@@ -21,12 +24,44 @@ public class List {
 	
 	public static void exec(Player player, String[] args) {
 		if(perms.has(player, "giantshop.api.web.list")) {
-			Set<String> tA = gA.getTrustedApps();
+			Map<String, ShopSender> tA = gA.getTrustedAppsMap();
 			
 			if(tA.size() > 0) {
-				for(String app : tA) {
-					// Nice and unclean for now :)
-					Heraut.say(player, app);
+				int perPage = config.Obtain().getInt("GiantShop.global.perPage");
+				int curPag = 0;
+
+				if(args.length >= 1) {
+					try{
+						curPag = Integer.parseInt(args[0]);
+					}catch(Exception e) {
+						curPag = 1;
+					}
+				}else
+					curPag = 1;
+
+				curPag = (curPag > 0) ? curPag : 1;
+				int pages = ((int)Math.ceil((double)tA.size() / (double)perPage) < 1) ? 1 : (int)Math.ceil((double)tA.size() / (double)perPage);
+				int start = (curPag * perPage) - perPage;
+				
+				if(curPag > pages) {
+					HashMap<String, String> d = new HashMap<String, String>();
+					d.put("list", "trusted app");
+					d.put("pages", String.valueOf(pages));
+					Heraut.say(player, mH.getMsg(Messages.msgType.ERROR, "pageOverMax", d));
+				}else{
+					HashMap<String, String> d = new HashMap<String, String>();
+					d.put("page", String.valueOf(curPag));
+					d.put("pages", String.valueOf(pages));
+					Heraut.say(player, mH.getMsg(Messages.msgType.MAIN, "TrustedAppPageHead", d));
+					for(Map.Entry<String, ShopSender> app : tA.entrySet()) {
+						d = new HashMap<String, String>();
+						d.put("page", String.valueOf(curPag));
+						d.put("pages", String.valueOf(pages));
+						d.put("appName", app.getKey());
+						d.put("appURI", app.getValue().getHostURI());
+						
+						Heraut.say(player, mH.getMsg(Messages.msgType.MAIN, "TrustedAppListEntry", d));
+					}
 				}
 			}else{
 				Heraut.say(player, mH.getMsg(Messages.msgType.ERROR, "noWebApps"));
