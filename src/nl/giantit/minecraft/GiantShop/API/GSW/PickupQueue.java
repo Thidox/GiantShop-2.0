bae5ffb3ce8f2a405e0dd530df5245eee11b8ffb
Giant <giant@tpre.nl>
2012-12-11 05:40:22 +0100

    Implement start of PickupQueue.

---
+++ src/nl/giantit/minecraft/GiantShop/API/GSW/PickupQueue.java
@@ -0,0 +1,68 @@
+package nl.giantit.minecraft.GiantShop.API.GSW;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import nl.giantit.minecraft.GiantShop.GiantShop;
+import nl.giantit.minecraft.GiantShop.core.Database.drivers.iDriver;
+
+/**
+ *
+ * @author Giant
+ */
+public class PickupQueue {
+	
+	private GiantShop p;
+	private HashMap<String, Queued> queue = new HashMap<String, Queued>();
+	
+	public PickupQueue(GiantShop p) {
+		this.p = p;
+	}
+	
+	public void addToQueue(String transactionID, String player, int amount, int id, int type) {
+		// Add purchase to database and take money
+		iDriver db = p.getDB().getEngine();
+		ArrayList<String> fields = new ArrayList<String>() {
+			{
+				add("transactionID");
+				add("player");
+				add("amount");
+				add("itemID");
+				add("itemType");
+			}
+		};
+		
+		HashMap<Integer, HashMap<String, String>> values = new HashMap<Integer, HashMap<String, String>>();
+		int i = 0;
+		
+		for(String field : fields) {
+			HashMap<String, String> temp = new HashMap<String, String>();
+			if(field.equals("transactionID")) {
+				temp.put("data", "'" + transactionID + "'");
+				values.put(i, temp);
+			}else if(field.equals("player")) {
+				temp.put("data", player);
+				values.put(i, temp);
+			}else if(field.equals("amount")) {
+				temp.put("data", "" + amount);
+				values.put(i, temp);
+			}else if(field.equals("itemID")) {
+				temp.put("data", "" + id);
+				values.put(i, temp);
+			}else if(field.equals("itemType")) {
+				temp.put("kind", "INT");
+				temp.put("data", "" + type);
+				values.put(i, temp);
+			}
+			
+			i++;
+		}
+		
+		// Insert transaction into database as ready pickup!
+		db.insert("#__api_gsw_pickups", fields, values).updateQuery();
+	}
+	
+	public boolean inQueue(String player) {
+		
+		return false;
+	}
+}
