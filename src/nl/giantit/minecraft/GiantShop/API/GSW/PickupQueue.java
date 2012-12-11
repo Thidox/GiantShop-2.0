f369593bc717d4b2ef4e668a1e222659826d66f4
Giant <giant@tpre.nl>
2012-12-11 06:59:35 +0100

    Continue work on the PickupQueue.

--- src/nl/giantit/minecraft/GiantShop/API/GSW/PickupQueue.java
+++ src/nl/giantit/minecraft/GiantShop/API/GSW/PickupQueue.java
@@ -2,8 +2,16 @@ package nl.giantit.minecraft.GiantShop.API.GSW;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import nl.giantit.minecraft.GiantShop.API.conf;
 import nl.giantit.minecraft.GiantShop.GiantShop;
+import nl.giantit.minecraft.GiantShop.Misc.Heraut;
+import nl.giantit.minecraft.GiantShop.Misc.Messages;
 import nl.giantit.minecraft.GiantShop.core.Database.drivers.iDriver;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.MaterialData;
 
 /**
  *
@@ -12,10 +20,13 @@ import nl.giantit.minecraft.GiantShop.core.Database.drivers.iDriver;
 public class PickupQueue {
 	
 	private GiantShop p;
+	private Messages mH;
+	
 	private HashMap<String, ArrayList<Queued>> queue = new HashMap<String, ArrayList<Queued>>();
 	
 	public PickupQueue(GiantShop p) {
 		this.p = p;
+		this.mH = p.getMsgHandler();
 	}
 	
 	public void addToQueue(String transactionID, String player, int amount, int id, int type) {
@@ -59,10 +70,72 @@ public class PickupQueue {
 		
 		// Insert transaction into database as ready pickup!
 		db.insert("#__api_gsw_pickups", fields, values).updateQuery();
+		
+		ArrayList<Queued> q;
+		if(this.queue.containsKey(player)) {
+			q = this.queue.remove(player);
+		}else{
+			q = new ArrayList<Queued>();
+		}
+		
+		Queued Q = new Queued(id, type, amount, transactionID);
+		q.add(Q);
+		this.queue.put(player, q);
+		
+		this.stalkUser(player);
 	}
 	
 	public boolean inQueue(String player) {
 		
 		return false;
+	}
+	
+	public void stalkUser(String player) {
+		Player pl = p.getSrvr().getPlayerExact(player);
+		if(null != pl) {
+			// Player is online! Bug him about his new purchase now!
+			conf c = GSWAPI.getInstance().getConfig();
+			if(c.getBoolean("GiantShopWeb.Items.PickupsRequireCommand")) {
+				Heraut.say(pl, mH.getMsg(Messages.msgType.MAIN, "itemWaitingForPickup"));
+				Heraut.say(pl, mH.getMsg(Messages.msgType.MAIN, "itemPickupHelp"));
+			}else{
+				this.deliver(pl);
+			}
+		}
+	}
+	
+	public void deliver(Player player) {
+		Inventory inv = player.getInventory();
+		ArrayList<Queued> qList = this.queue.get(player.getName());
+		for(Queued q : qList) {
+			HashMap<String, String> data = new HashMap<String, String>();
+			data.put("transactionID", q.getTransactionID());
+			data.put("itemID", String.valueOf(q.getItemID()));
+			data.put("itemType", String.valueOf(q.getItemType()));
+			data.put("itemName", q.getItemName());
+			data.put("amount", String.valueOf(q.getAmount()));
+			
+			Heraut.say(player, mH.getMsg(Messages.msgType.MAIN, "itemDelivery", data));
+			
+			ItemStack iStack;
+			if(q.getItemType() != -1 && q.getItemType() != 0) {
+				if(q.getItemID() != 373)
+					iStack = new MaterialData(q.getItemID(), (byte) ((int) q.getItemType())).toItemStack(q.getAmount());
+				else
+					iStack = new ItemStack(q.getItemID(), q.getAmount(), (short) ((int) q.getItemType()));
+			}else{
+				iStack = new ItemStack(q.getItemID(), q.getAmount());
+			}
+			
+			HashMap<Integer, ItemStack> left = inv.addItem(iStack);
+			if(!left.isEmpty()) {
+				Heraut.say(player, mH.getMsg(Messages.msgType.ERROR, "infFull"));
+				for(Map.Entry<Integer, ItemStack> stack : left.entrySet()) {
+					player.getWorld().dropItem(player.getLocation(), stack.getValue());
+				}
+			}
+		}
+		
+		Heraut.say(player, mH.getMsg(Messages.msgType.MAIN, "itemsDelivered"));
 	}
 }
