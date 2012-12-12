fd58afa68cb3aed909e9e909270243d95035ca1f
Giant <giant@tpre.nl>
2012-12-12 08:27:58 +0100

    Fix issue where ItemID Obj and ItemName were always "null".

--- src/nl/giantit/minecraft/GiantShop/API/GSW/Queued.java
+++ src/nl/giantit/minecraft/GiantShop/API/GSW/Queued.java
@@ -11,12 +11,14 @@ public class Queued {
 	
 	private int id;
 	private int type;
+	private Integer itemType;
 	private int amount;
 	private String transactionID;
 	
 	public Queued(int id, int type, int amount, String transactionID) {
 		this.id = id;
 		this.type = type;
+		this.itemType = (type <= 0) ? null : type;
 		this.amount = amount;
 		this.transactionID = transactionID;
 	}
@@ -30,11 +32,11 @@ public class Queued {
 	}
 	
 	public ItemID getItemIDOBJ() {
-		return GiantShop.getPlugin().getItemHandler().getItemIDByName(GiantShop.getPlugin().getItemHandler().getItemNameByID(this.id, this.type));
+		return GiantShop.getPlugin().getItemHandler().getItemIDByName(GiantShop.getPlugin().getItemHandler().getItemNameByID(this.id, this.itemType));
 	}
 	
 	public String getItemName() {
-		return GiantShop.getPlugin().getItemHandler().getItemNameByID(this.id, this.type);
+		return GiantShop.getPlugin().getItemHandler().getItemNameByID(this.id, this.itemType);
 	}
 	
 	public int getAmount() {
