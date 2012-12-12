475770797e59d43618fb0721a1d2a0b96f93e186
Giant <giant@tpre.nl>
2012-12-12 08:29:41 +0100

    Add actuall pickup command handling. Add help command to handler.

--- src/nl/giantit/minecraft/GiantShop/API/GSW/Commands/ChatExecutor.java
+++ src/nl/giantit/minecraft/GiantShop/API/GSW/Commands/ChatExecutor.java
@@ -20,10 +20,14 @@ public class ChatExecutor {
 			String cmd = args[0];
 			args = Arrays.copyOfRange(args, 1, args.length);
 			
-			if(Misc.isAnyIgnoreCase(cmd, "list", "li", "l")) {
+			if(Misc.isAnyIgnoreCase(cmd, "help", "hel", "he", "h", "?")) {
+				Help.showHelp(player, args);
+			}else if(Misc.isAnyIgnoreCase(cmd, "list", "li", "l")) {
 				List.exec(player, args);
 			}else if(Misc.isAnyIgnoreCase(cmd, "register", "reg", "r")) {
 				Register.exec(player, args);
+			}else if(Misc.isAnyIgnoreCase(cmd, "pickup", "pick", "pu", "p")) {
+				Pick.exec(player, args);
 			}else{
 				// admin command stuff
 			}
@@ -31,6 +35,7 @@ public class ChatExecutor {
 			Heraut.say(player, "Ok, we have no friggin clue what you are on about, so what about we just send you our help page?");
 			Help.showHelp(player, args);
 		}
+		
 		return true;
 	}
 }
